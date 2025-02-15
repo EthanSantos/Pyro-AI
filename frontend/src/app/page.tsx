871f04334adecff369a011681e2ee71057f4ee71
EthanSantos <ethanangelo61@gmail.com>
2025-02-15 13:47:20 -0800

    test deployment

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-8">
       <h1 className="text-3xl font-bold mb-4">
-        Home page
+        Home Page
       </h1>
     </div>
   );
