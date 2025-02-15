4a0b0f2895da8bcaeb1a20c269866101b5c5882d
kellyy8 <kellyyu2886@gmail.com>
2025-02-15 12:54:57 -0800

    Added mock news to display news cards on home page.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import NewsCard from "@/components/ui/NewsCard"
 
 export default function Home() {
@@ -22,7 +22,7 @@ export default function Home() {
       source: "National Weather Service",
       date: "30 minutes ago",
       content: "Weather service warns of strong winds up to 45mph, creating dangerous fire conditions in affected areas.",
-      link: null,
+      link: "https://www.google.com/search?q=pigs&rlz=1C5CHFA_enUS973US973&oq=pigs&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg8MgYIAhBFGDzSAQc1MTNqMGo3qAIIsAIB&sourceid=chrome&ie=UTF-8#vhid=NxbLQ-iqV2JCnM&vssid=l",
     },
     {
       id: 3,
@@ -35,24 +35,37 @@ export default function Home() {
     },
   ]
 
-  const [filter, setFilter] = useState<String | null>(null)
-  const [filteredNews, setFilteredNews] = useState<Array<{id: number, title: String, tag: String, source: String, date: String, content: String, link: String | null}>>(allNews)
+  const [filter, setFilter] = useState<String | null>(null)  // TODO: Implement filter functionality.
+  const [displayedNews, setDisplayedNews] = useState<Array<{id: number, title: String, tag: String, source: String, date: String, content: String, link: String}>>(allNews)
+
+  useEffect(() => {
+    if (filter) {
+      const filteredNews = allNews.filter((news) => news.tag === filter)
+      setDisplayedNews(filteredNews)
+    } else {
+      setDisplayedNews(allNews)
+    }
+
+  }, [filter])
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-8">
+    <div className="min-h-screen flex flex-col items-center justify-center p-8 space-y-4">
       <h1 className="text-3xl font-bold mb-4">
         [PLACEHOLDER] Next.js App Router with Axios, shadcn UI & Flask
       </h1>
-      
-      <NewsCard 
-        title="New Evacuation Orders Issued for Northern California"
-        tag="Evacuations"
-        source="CalFire"
-        date="5 minutes ago"
-        content="Mandatory evacuations have been ordered for residents in the Paradise Pine area due to rapidly spreading wildfire."
-        link="https://example.com/wildfire.jpg"
-      />
-      
+
+      {displayedNews.map((news) => (
+        <NewsCard
+          key={news.id}
+          title={news.title}
+          tag={news.tag}
+          source={news.source}
+          date={news.date}
+          content={news.content}
+          link={news.link}
+        />
+      ))}
+
     </div>
-  );
+  )
 }
